922f313f46debf3bbbe78c7cb75063f7432c7a14
Glitch (glitch-hello-node) <none>
2021-09-29 13:33:53 +0000

    🐞🍕 Checkpoint ./server.js:36836277/377 ./package.json:36836277/2202

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
 
 const path = require("path");
 
-// Require the fastify framework and instantiate it
 const fastify = require("fastify")({
-  // Set this to true for detailed logging:
   logger: true
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// fastify-formbody lets us parse incoming forms
 fastify.register(require("fastify-formbody"));
 
-
 fastify.register(require("fastify-cors"), {
-  origin: /^.*?\.silashop\.(com|co\.il)$/,
-  methods: ["GET"]
+  origin: /^.*?\.silashop\.(com|co\.il)$/
 });
 
 fastify.get("/hello", function(request, reply) {

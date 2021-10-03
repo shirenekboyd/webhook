72cb81e05f34a9de8caf57a7dd859d727f561581
Dor Ganot <dorespoog@gmail.com>
2021-10-03 13:10:07 +0300

    Test commit

--- routes/group-orders/routes.js
+++ routes/group-orders/routes.js
@@ -1,3 +1,15 @@
+const client = require('@sendgrid/client');
+client.setApiKey(process.env.SENDGRID_API_KEY);
+const request = {
+  method: 'GET',
+  url: '/v3/api_keys'
+};
+client.request(request)
+.then(([response, body]) => {
+  console.log(response.statusCode);
+  console.log(body);
+})
+
 async function routes(fastify, options){
     fastify.post('/subscribe-to-availability', async function(request, reply) {
          return {email: request.body.email, productSKU: request.body.productSKU};

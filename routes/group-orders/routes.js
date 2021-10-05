9acb160faa865598f91a42a79f36f7f488659a8f
Glitch (sila-server) <none>
2021-10-05 20:17:19 +0000

    Import of DoresPoogSila/sila-server-glitch

--- routes/group-orders/routes.js
+++ routes/group-orders/routes.js
@@ -1,13 +1,11 @@
 const sendGrid = require('../../external-services/sendgrid');
 
 async function routes(fastify, options) {
-    fastify.post('/subscribe-to-availability', async function(request, reply) {
+    fastify.post('/subscribe-to-product', async function(request, reply) {
         const email = request.body.email;
         const productSKU = request.body.productSKU;
         const listName = `Group Order Notification - SKU ${productSKU}`;
-      console.log('getting contact by email');
         const getContactPromise = sendGrid.getContactByEmail(email);
-      console.log('getting list by name');
         const getListPromise = sendGrid.getListByName(listName).then((list) => {
             return list || sendGrid.createList(listName);
         });

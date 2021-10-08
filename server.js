77f605245025a97fd589cb71e212afebc7cb31ee
Glitch (sila-server) <none>
2021-10-08 17:51:16 +0000

    Import of DoresPoogSila/sila-server-glitch ./package.json:36836277/1263 ./external-services/sendgrid/sendgrid-client-proxy.js:36836277/1513

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ fastify.register(autoload, {
 });
 
 fastify.register(cors, {
-  origin: [/^.*?\.silashop\.(com|co\.il)$/, /^.*?\.zapier\.com$/]
+  origin: /^.*?\.silashop\.(com|co\.il)$/
 });
 
 // Run the server and report out to the logs

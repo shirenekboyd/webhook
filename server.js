0f0e39c444c73823b13a98b847bbd17ae794e443
Glitch (hammerhead-utopian-question) <none>
2022-02-25 04:09:24 +0000

    🍞🍣 Checkpoint ./server.js:42579260/1

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-
 const fastify = require("fastify")({
-  logger: true
+  logger: true,
 });
 
-
-fastify.register(require('fastify-formbody'));
-
+fastify.register(require("fastify-formbody"));
 
 fastify.route({
-  method: 'POST',
-  url: '/olark',
+  method: "POST",
+  url: "/olark",
   handler: function (request, reply) {
     /* your code here */
-    //reply.send({})
-    return {'status': 'no pizza', 'integrationUrl': undefined};
-  }
+    console.log(reply);
+    return { status: "no pizza", integrationUrl: undefined };
+  },
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
+fastify.listen(process.env.PORT, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
